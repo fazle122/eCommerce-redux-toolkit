1858794f38ffbc3af8a76e1a2b2b2218a2e7e5e5
fr <fazlerabby122@gmail.com>
2024-09-24 15:44:57 +0600

    base url fix

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,7 @@
 
 // export const BASE_URL = "";
-export const BASE_URL = "http://localhost:8000";
-export const BASE_URL_LOCAL= "https://e-ccommerce-nodejs.vercel.app";
+export const BASE_URL_LOCAL = "http://localhost:8000";
+export const BASE_URL= "https://e-ccommerce-nodejs.vercel.app";
 export const PRODUCTS_URL = `${BASE_URL}/api/products`;
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const ORDERS_URL = `${BASE_URL}/api/orders`;
